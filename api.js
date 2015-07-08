c4115b2db3902415e8015ad90badc65fc87a3abc
Aditya Hegde <adityahegderocks@gmail.com>
2015-07-08 08:19:49 -0700

    First pages commit

---
+++ api.js
@@ -0,0 +1,50 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EmberForm.CheckBoxView",
+        "EmberForm.DisableForColumn",
+        "EmberForm.DynamicMultiSelectView",
+        "EmberForm.DynamicSelectView",
+        "EmberForm.FileUploadView",
+        "EmberForm.FormColumnData",
+        "EmberForm.FormColumnDataGroup",
+        "EmberForm.FormComponent",
+        "EmberForm.GroupRadioButtonView",
+        "EmberForm.HideForColumn",
+        "EmberForm.LabelView",
+        "EmberForm.Legend",
+        "EmberForm.MultiColumnMixin",
+        "EmberForm.MultiEntryView",
+        "EmberForm.MultiInputView",
+        "EmberForm.MultipleValue",
+        "EmberForm.MultipleValueMixin",
+        "EmberForm.RadioInputView",
+        "EmberForm.StaticSelectView",
+        "EmberForm.TextAreaView",
+        "EmberForm.TextInputView",
+        "EmberForm.WrapperView"
+    ],
+    "modules": [
+        "ember-form",
+        "ember-form-column-data",
+        "ember-form-items"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-form",
+            "name": "ember-form",
+            "description": "A module for a form."
+        },
+        {
+            "displayName": "ember-form-column-data",
+            "name": "ember-form-column-data",
+            "description": "Base form view.\nUsage:\n\n    {{ember-form record=record columnDataGroup=columnDataGroup}}"
+        },
+        {
+            "displayName": "ember-form-items",
+            "name": "ember-form-items",
+            "description": "Module with all the form items."
+        }
+    ]
+} };
+});
